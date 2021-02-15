8a8e42d8c5fb25a14e04396a48c94af5f2f47c95
Steven Smail <41452253+ssmail7@users.noreply.github.com>
2021-02-15 16:26:54 -0500

    Update KeyboardToFile.java

--- KeyboardToFile.java
+++ KeyboardToFile.java
@@ -1,37 +1,61 @@
-//************************************************************
+//*************************************************************
 //  Author: Steven Smail
 //
 //  KeyboardToFile.java
 //
 //  This program reads an arbitrary number of strings from
-//  the keyboard and writes them to a text file.//************************************************************
+//  the keyboard and writes them to a text file.
+//*************************************************************
 import java.util.Scanner;
-import java.io.FileOutputStream;import java.io.PrintWriter;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
 import java.io.FileNotFoundException;
-public class KeyboardToFile{    public static void main( String[] args)    {
+
+public class KeyboardToFile
+{
+    public static void main( String[] args)
+    {
         String n, x = "";
-        System.out.println("This is program KeyboardToFile.");        System.out.println();
-        try        {
-            // Get filename from the user            System.out.print("Please enter name for file to be written: ");            Scanner keyboardScanner = new Scanner(System.in);            String filename = keyboardScanner.nextLine();
+
+        System.out.println("This is program KeyboardToFile.");
+        System.out.println();
+
+        try
+        {
+            // Get filename from the user
+            System.out.print("Please enter name for file to be written: ");
+            Scanner keyboardScanner = new Scanner(System.in);
+            String filename = keyboardScanner.nextLine();
 
             System.out.println("Enter strings to be written to the file.");
             System.out.println("Press Enter with no text to end the program.");
-            FileOutputStream fos =                 new FileOutputStream(filename, false);
+
+            FileOutputStream fos = 
+                new FileOutputStream(filename, false);
 
             PrintWriter pw = new PrintWriter(fos);
 
         do
         {
             System.out.print("Enter string: ");
-            Scanner stringScanner = new Scanner(System.in);            n = stringScanner.nextLine();
+            Scanner stringScanner = new Scanner(System.in);
+            n = stringScanner.nextLine();
             if (!(n.equals(x)))
             {
                 pw.println(n);
             }
         } while (!(n.equals(x)));
-            pw.close();
 
-            System.out.println("File " + filename + " written.");        }
-        catch (FileNotFoundException ex)        {            System.out.println("Failed to create output file.");        }        System.out.println("Program complete.");
+            pw.close();
+
+            System.out.println("File " + filename + " written.");
+        }
+
+        catch (FileNotFoundException ex)
+        {
+            System.out.println("Failed to create output file.");
+        }
+        System.out.println("Program complete.");
         System.out.println();
-    }}
+    }
+}
